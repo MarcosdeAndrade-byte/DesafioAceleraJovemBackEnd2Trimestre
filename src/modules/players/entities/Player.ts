0af2b6e24fdf5f95e7b67c9cf1f2bfa38c717369
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-27 13:14:30 -0300

    Feat: adicionando entidades Player e Team

---
+++ src/modules/players/entities/Player.ts
@@ -0,0 +1,18 @@
+class Player {
+    id: string;
+    team_id: string | null;
+    name: string;
+    age: number;
+    goals: number;
+
+    constructor(id: string, team_id: string | null, name: string, age: string, position: "Goalkeeper" | "Defender" | "Midfielder" | "Forward", goals: number) {
+        id = id;
+        team_id = team_id || null;
+        name = name;
+        age = age;
+        position = position;
+        goals = goals;
+    }
+}
+
+export { Player };
---
+++ src/modules/team/entities/Team.ts
@@ -0,0 +1,17 @@
+class Team {
+    id: string;
+    name: string;
+    coach: string;
+    stadium: string;
+    city: string;
+
+    constructor(id: string, name: string,coach: string, stadium: string, city: string) {
+        id = id;
+        name = name;
+        coach = coach;
+        stadium = stadium;
+        city = city;
+    }
+};
+
+export { Team };
