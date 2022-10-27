40415e91e2ae3e963dede561e4ee91aceed915db
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-27 17:04:44 -0300

    Feat: adicionando configuracoes do sqlite

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
-
+import { openDb } from "../../../../configDB";
 const app = express();
 
+openDb();
+
 const PORT = 3000;
-app.listen(PORT, () => {console.log(`SERVIDOR RODANDO NA PORTA ${PORT}`)});
+app.listen(PORT, () => { console.log(`SERVIDOR RODANDO NA PORTA ${PORT}`) });
