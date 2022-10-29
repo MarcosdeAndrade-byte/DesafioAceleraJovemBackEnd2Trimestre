71cd44b9a917acaf6b49723d8d0bbb65793bcf2c
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-28 23:32:43 -0300

    Feat: adicionando metodo para adicionar Time

--- src/modules/team/infra/repositories/ITeamsRepository.ts
+++ src/modules/team/infra/repositories/ITeamsRepository.ts
@@ -1,5 +1,8 @@
+import { ITeamDTO } from "../../../dtos/ITeamDTO";
+
 interface ITeamsRepository {
     createTable(): Promise<void>;
+    addTeam({id,name,coach,stadium,city}: ITeamDTO): Promise<void>;
 }
 
 export { ITeamsRepository };
--- src/modules/team/infra/repositories/implementations/TeamsRepository.ts
+++ src/modules/team/infra/repositories/implementations/TeamsRepository.ts
@@ -1,4 +1,5 @@
 import { openDb } from "../../../../../../configDB";
+import { ITeamDTO } from "../../../../dtos/ITeamDTO";
 import { ITeamsRepository } from "../ITeamsRepository";
 
 class TeamsRepository implements ITeamsRepository {
@@ -8,6 +9,12 @@ class TeamsRepository implements ITeamsRepository {
     async createTable(): Promise<void> {
         openDb().then(db => {
             db.exec("CREATE TABLE IF NOT EXISTS teams_repository (id TEXT,name TEXT,coach TEXT,stadium TEXT,city TEXT)");
+        });
+    }
+
+    async addTeam({id,name,coach,stadium,city}: ITeamDTO): Promise<void> {
+        openDb().then(db => {
+            db.run("INSERT INTO teams_repository (id,name,coach,stadium,city) VALUES (?,?,?,?,?)",id,name,coach,stadium,city);
         });
     }
 
