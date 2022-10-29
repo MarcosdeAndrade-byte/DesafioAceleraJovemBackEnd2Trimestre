da115604e3b3eafe143c26d6f46a7131a61de1f7
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-29 17:48:36 -0300

    Refactor: normalizando rotas de criacao e contagem por gols

--- src/shared/routes/players.routes.ts
+++ src/shared/routes/players.routes.ts
@@ -1,4 +1,4 @@
-import { response, Router } from "express";
+import { Router } from "express";
 import { createPlayerController } from "../../modules/players/useCases/createPlayer";
 import { listPlayerByIdController } from "../../modules/players/useCases/listPlayerById";
 import { playersByGoalsController } from "../../modules/players/useCases/playersByGoals";
@@ -7,9 +7,9 @@ import { sumOfGolsController } from "../../modules/players/useCases/sumOfGols";
 
 const playersRoutes = Router();
 
-playersRoutes.post("/", (request, response) => createPlayerController.handle(request, response));
+playersRoutes.put("/", (request, response) => createPlayerController.handle(request, response));
 playersRoutes.get("/", (request, response) => listPlayerByIdController.handle(request, response));
-playersRoutes.get("/playersbygoals", (request, response) => playersByGoalsController.handle(request, response));
+playersRoutes.get("/goals", (request, response) => playersByGoalsController.handle(request, response));
 playersRoutes.get("/sum", (request, response) => sumOfGolsController.handle(request, response));
 playersRoutes.get("/positions", (request,response) => separatedByPositionController.handle(request, response));
 
