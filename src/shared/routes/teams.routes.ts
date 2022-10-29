e373dd75b8d4b59b2c1636d8427d6c243352db28
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-29 10:26:37 -0300

    Feat: adicionando rota de listagem dos Times no arquivo teams.routes

--- src/shared/routes/teams.routes.ts
+++ src/shared/routes/teams.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createTeamController } from "../../modules/team/useCases/createTeam";
+import { listTeamsController } from "../../modules/team/useCases/listTeams";
 
 
 const teamsRoutes = Router();
 
 teamsRoutes.post("/", (request, response) => { createTeamController.handle(request, response); });
+teamsRoutes.get("/", (request, response) => { listTeamsController.handle(request, response); });
 
 export { teamsRoutes };
