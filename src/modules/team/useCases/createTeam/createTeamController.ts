857fb8421b9b6431953cf2a66be5ddb8d755d807
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-30 20:57:29 -0300

    Fix: retirando tratamento de erro incorreto na rota createTeamController

--- src/modules/team/useCases/createTeam/createTeamController.ts
+++ src/modules/team/useCases/createTeam/createTeamController.ts
@@ -7,9 +7,6 @@ class CreateTeamController {
     async handle(request: Request, response: Response): Promise<Response>{
         try {
            const {id,name,coach,stadium,city} = request.body;
-           if(id || name || coach || stadium || city == undefined) {
-                throw new Error("Para adicionar um time adicione todos os dados: id,name,coach,stadium,city");
-           }
            await this.createTeamUseCase.execute({id,name,coach,stadium,city});
            return response.status(200).json("OK");
         } catch (error) {
