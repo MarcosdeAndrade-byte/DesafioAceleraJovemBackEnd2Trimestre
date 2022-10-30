d1178271360df629219747cb71ab70e5e02f3170
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-30 17:18:45 -0300

    Fix: corrigindo status do tratamento de erro na rota createPlayer

--- src/modules/players/useCases/createPlayer/createPlayerController.ts
+++ src/modules/players/useCases/createPlayer/createPlayerController.ts
@@ -10,7 +10,7 @@ class CreatePlayerController {
            await this.createPlayerUseCase.execute({id,team_id,name,age,position,goals});
            return response.json("OK").status(200);
         } catch (error) {
-            return response.status(401).json(error.message);
+            return response.status(400).json(error.message);
         }
     }
 
