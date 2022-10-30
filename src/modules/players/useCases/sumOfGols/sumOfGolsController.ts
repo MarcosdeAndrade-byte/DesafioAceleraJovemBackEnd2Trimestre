0f2b4d8424fbeb609fdceb8caadcb10520d611c0
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-30 15:57:23 -0300

    Fix: alterando cabecalho para acesso do Id no sumOfGolsController

--- src/modules/players/useCases/sumOfGols/sumOfGolsController.ts
+++ src/modules/players/useCases/sumOfGols/sumOfGolsController.ts
@@ -7,7 +7,7 @@ class SumOfGolsController {
     constructor(private sumOfGolsUseCase: SumOfGolsUseCase) { }
 
     async handle(request: Request, response: Response): Promise<Response> {
-        const { team_id } = request.body;
+        const team_id = request.headers.team_id as any;
         const sumOfGols = await this.sumOfGolsUseCase.execute(team_id);
         return response.status(200).json(sumOfGols);
     }
